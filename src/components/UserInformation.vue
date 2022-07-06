7ecb81dac35c71810c0f505d39e4f5569970b245
Claudia <claudia.levy@pendo.io>
2022-07-06 17:05:31 +0300

    Updated: HelloWord component

---
+++ src/components/UserInformation.vue
@@ -0,0 +1,34 @@
+<template>
+  <section id="assignment">
+    <h2>{{ name }}</h2>
+    <p>{{ age }}</p>
+    <p>{{ age + 5 }}</p>
+    <p>Favorite Number: {{ randomNumber }}</p>
+    <input :value="name" type="text"/>
+    <div>
+      <img src="https://i2-prod.mirror.co.uk/incoming/article25609246.ece/ALTERNATES/s1200d/0_PUSS-IN-BOOTS.jpg"/>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: "Claudia",
+      age: 28,
+      randomNumber: Math.random() * 1
+    };
+  },
+  methods: {},
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+img {
+  width: 200px;
+  height: 200px;
+  margin-top: 10px;
+}
+</style>
