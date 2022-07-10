f2a77da1a6c6244d9cfe11976a8984f9d7ca030d
Claudia <claudia.levy@pendo.io>
2022-07-10 16:20:29 +0300

    Added: new components

---
+++ src/components/ActionReactivity.vue
@@ -0,0 +1,50 @@
+<template>
+  <section id="assignment">
+    <button @click="incrementBy(5)">Add 5</button>
+    <button @click="incrementBy(1)">Add 1</button>
+    <!-- 1) Connect the buttons and calculate a value (a number) -->
+    <!-- Show "Not there yet" until you reach a result of exactly 37 -->
+    <!-- Show "Too much!" if the result is greater than 37 -->
+    <p>Result: {{ value }}</p>
+    <bold>{{ getText }}</bold>
+    <!-- 2) Watch for changes in "result" and reset the value to 0 after 5 seconds -->
+  </section>
+</template>
+
+<script>
+export default {
+  name: "ActionReactivity",
+  data() {
+    return {
+      value: 0,
+      text: ""
+    };
+  },
+  methods: {
+    incrementBy(val) {
+      this.value += val;
+    }
+  },
+  computed: {
+    getText() {
+      if (this.value > 37)
+        return "Too much!";
+      else if (this.value < 37)
+        return "Not there yet"
+      return "Exact value !!!"
+    }
+  },
+  watch: {
+    value() {
+      setTimeout(() => {
+        this.value = 0;
+      }, 3000);
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/ClassBinding.vue
@@ -0,0 +1,57 @@
+<template>
+  <section>
+    <!-- 1) Fetch the user input and use it as a CSS class -->
+    <!-- The entered class should be added to the below paragraph -->
+    <!-- 2) Use the "visible" and "hidden" classes to show/ hide the above paragraph -->
+    <div class="container">
+      <input v-model="className" type="text"/>
+      (available classes: "user1", "user2")
+      <button @click="show = !show">Toggle Paragraph</button>
+      <p v-show="show" :class="className">
+        Style me!
+      </p>
+    </div>
+
+    <!-- Clicking the button should toggle between the two options -->
+
+    <!-- 3) Add dynamic inline styling to the below paragraph and let the user enter a background-color -->
+    <div class="container">
+      <input v-model="bgColor" type="text"/>
+      <p :style="{'background-color': bgColor }">Style me inline!</p>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "ClassBinding",
+  data() {
+    return {
+      className: "",
+      bgColor: "",
+      show: true
+    };
+  },
+  methods: {},
+  computed: {}
+}
+</script>
+
+<style scoped>
+.user1 {
+  color: red;
+}
+
+.user2 {
+  color: green;
+}
+
+.invisible {
+  visibility: hidden;
+}
+
+.container {
+  width: 400px;
+  height: 100px;
+}
+</style>
